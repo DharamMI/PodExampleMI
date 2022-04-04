c366305776e7b578a93beb46f896ad38a2672ac9
Dharamraj <dharamrajsinh.jadeja@mindinventory.com>
2022-04-04 12:39:29 +0530

    - Added test cases and created swift file.

---
+++ PodExampleMI/PodExampleMI.swift
@@ -0,0 +1,31 @@
+//
+//  PodExampleMI.swift
+//  PodExampleMI
+//
+//  Created by Mac-0004 on 04/04/22.
+//
+
+import Foundation
+
+
+public final class PodExampleMI {
+
+    let name = "PodExampleMI"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func multiple(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    public func division(a: Int, b: Int) -> Int  {
+        return a / b
+    }
+    
+}
