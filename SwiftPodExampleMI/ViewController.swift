87f373da856ed06b19f487fe4151404b9be7597a
Dharamraj <dharamrajsinh.jadeja@mindinventory.com>
2022-04-04 11:52:35 +0530

    Initial project setup

---
+++ SwiftPodExampleMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPodExampleMI
+//
+//  Created by Mac-0004 on 04/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
