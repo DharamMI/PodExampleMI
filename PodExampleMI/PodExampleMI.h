e85887e3f3a7f093daa9f331084cb7214f7bbd69
Dharamraj <dharamrajsinh.jadeja@mindinventory.com>
2022-04-04 11:29:16 +0530

    Initial Commit

---
+++ PodExampleMI/PodExampleMI.h
@@ -0,0 +1,18 @@
+//
+//  PodExampleMI.h
+//  PodExampleMI
+//
+//  Created by Mac-0004 on 04/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodExampleMI.
+FOUNDATION_EXPORT double PodExampleMIVersionNumber;
+
+//! Project version string for PodExampleMI.
+FOUNDATION_EXPORT const unsigned char PodExampleMIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodExampleMI/PublicHeader.h>
+
+
